31d89d8b95ce02b88f7ffdb81f0af42976dca48f
levspb666 <levspb666@yandex.ru>
2018-04-20 12:04:48 +0300

    HW7_task_1

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,6 +2,9 @@ package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -23,6 +26,23 @@ public class RootControllerTest extends AbstractControllerTest {
                         allOf(
                                 hasProperty("id", is(START_SEQ)),
                                 hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+    @Test
+    public void testMeals() throws Exception{
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ+2)),
+                                hasProperty("dateTime", is(LocalDateTime.of(2015, Month.MAY, 30, 10, 0))),
+                                hasProperty("description", is("Завтрак")),
+                                hasProperty("calories", is(500))
                         )
                 )));
     }
